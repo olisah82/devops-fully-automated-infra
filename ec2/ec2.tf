9a4a3d8e663759565c60e0372b7099d67c177c3e
olisah mbaeyi <olisah0882@gmail.com>
2022-12-14 21:21:26 -0500

    new code improvement

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -20,7 +20,7 @@ variable "key_pair_name" {
 }
 
 resource "aws_security_group" "ec2_sg" {
-  name        = join("", [var.name, "-", "ec2-sg"])
+  name        = join("", [var.name, "dev-sg", "ec2-sg"])
   description = "Allow  traffic for http and ssh"
 
 
@@ -28,28 +28,28 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/0"]
   }
 
   ingress {
     from_port   = 9100
     to_port     = 9100
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/0"]
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/0"]
   }
 }
 
